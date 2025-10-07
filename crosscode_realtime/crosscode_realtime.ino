ca5fc8c374b76fe2806ec9cc0fc7c9ad3fd9239e
anowlen <anowlen@usc.edu>
2025-10-07 13:26:21 -0700

    trying to do cross correlation function

--- crosscode_realtime/crosscode_realtime.ino
+++ crosscode_realtime/crosscode_realtime.ino
@@ -8,93 +8,17 @@ float sensorData1[bufSize];
 float sensorData2[bufSize];
 int index = 0;
 
+struct crossCorrReturn{
+  float highr;
+  float highshift;
+};
+
+
 
 void setup() {
     Serial.begin(115200);
-    
-
-pinMode(A0,INPUT);
-pinMode(A1,INPUT);
-
-
-
-    
-    /*
-    int n = sizeof(x) / sizeof(x[0]); 
-
-    float mx = 0, my = 0, sx = 0, sy = 0, sxy, r;
-    
-    // Calculate mean
-    for (int i = 0; i < n; i++) {
-        mx += x[i];
-        my += y[i];
-    }
-    mx /= n;
-    my /= n;
-
-    // calculate denominator
-    for (int i = 0; i < n; i++) {
-        sx += (x[i] - mx) * (x[i] - mx);
-        sy += (y[i] - my) * (y[i] - my);
-    }
-    double denom = sqrt(sx * sy);
-
-    //int maxShift = n / 2;  // Maximum shift range ->have to divide by 2
-    int maxShift2 = 100; //this makes it match the matlab
-    const float Ts = 0.01; //sample rate
-    Serial.println("delay,correlation");  //Serial Plotter
-
-    float highr = -1.0;
-    float highshift = 0;
-    // Calculate and print cross-correlation
-    for (int shift = -maxShift2; shift <= maxShift2; shift++) { //og maxShift
-        sxy = 0;
-        
-        for (int i = 0; i < n; i++) {
-            int j = i + shift;
-            if (j < 0 || j >= n) continue;
-            //if (j < 0 || j >= n){
-              //sxy += (x[i] - mx) * (-my);
-            //}
-            else{
-              sxy += (x[i] - mx) * (y[j] - my); 
-            }
-            
-        }
-        
-        r = sxy / denom;
-        //Serial.print("Denom: ");
-        //Serial.println(denom);
-
-        if (r > highr){
-          highr = r;
-          highshift = shift *Ts; //og just shift
-        }
-        float delaySec = shift * Ts; //og not here
-        //shift = shift * .01;
-        // serial plotter show shift and cros correlation
-        //Serial.print((delaySec));//unshow this on the graph, og shift
-        /*
-        Serial.print(shift*Ts); //same as line above, trying to save SRAM
-        Serial.print(F(","));  
-        Serial.println(r,3);  // Correlation coefficient (y-axis)
-        // originally there was the end big comment thing here with the /
-        
-        
-        Serial.print(delaySec, 5);
-        Serial.print(',');
-        Serial.println(r, 5);
-        delay(5);
-        
-
-        delay(100); 
-    }
-    Serial.print(F("highest r: "));
-    Serial.println(highr,5);
-    Serial.println(F("corresponding shift: "));
-    Serial.println(highshift,5);
-    */
-
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
 
 }
 
@@ -111,27 +35,156 @@ void loop() {
   index++;
   Serial.println(index);
   Serial.println(currentData1,6);
-  
-  
-
-
-
 
   if (index >= bufSize){
-    float average = average_calculate(sensorData1, bufSize); 
+    /*float average = average_calculate(sensorData1, bufSize); 
     //once index reaches the full buffer, then it calculates the average and prints it out
     Serial.println(average);
+    */
 
-    
+    crossCorrReturn crossCorr_calculate(sensorData1, sensorData2, bufSize);
+    Serial.println(crossCorrReturn.highshift);
+    Serial.println(crossCorrReturn.highr);
+
     index = 0; //am i clearing this properly
     // once it reaches the buffer size, then it has to collect new data and start over again
   }
-
   delay(10);
 }
 
-void crossCorr(){
 
+
+
+crossCorrReturn crossCorr_calculate(float x[], float y[], int n){ //two arrays, and the buffersize
+  
+  crossCorrReturn result;
+
+  //this is all just copied over from other cross correlation
+  float mx = 0, my = 0, sx = 0, sy = 0, sxy, r;
+    
+    // Calculate mean
+    for (int i = 0; i < n; i++) {
+        mx += x[i];
+        my += y[i];
+    }
+    mx /= n;
+    my /= n;
+
+    // calculate denominator
+    for (int i = 0; i < n; i++) {
+        sx += (x[i] - mx) * (x[i] - mx);
+        sy += (y[i] - my) * (y[i] - my);
+    }
+    double denom = sqrt(sx * sy);
+
+    int maxShift2 = 100; //this makes it match the matlab
+    const float Ts = 0.01; //sample rate
+    Serial.println("delay,correlation");  //Serial Plotter
+
+
+    float highr = -1.0;
+    float highshift = 0;
+    // Calculate and print cross-correlation
+    for (int shift = -maxShift2; shift <= maxShift2; shift++) { //og maxShift
+        sxy = 0;
+
+        for (int i = 0; i < n; i++) {
+            int j = i + shift;
+            if (j < 0 || j >= n) continue;
+            //if (j < 0 || j >= n){
+              //sxy += (x[i] - mx) * (-my);
+            //}
+            else{
+              sxy += (x[i] - mx) * (y[j] - my); 
+            }
+            
+        }
+
+        r = sxy / denom;
+
+        if (r > highr){
+          highr = r;
+          highshift = shift *Ts; //og just shift
+        }
+        float delaySec = shift * Ts; //og not here
+
+        Serial.print(delaySec, 5);
+        Serial.print(',');
+        Serial.println(r, 5);
+        delay(5);
+        
+      //need to return highr, highshift
+
+        delay(100); 
+
+    result.highr = highr;
+    result.highshift = highshift;
+    return result;
+        
+}
+
+
+float crossCorr_calculate(float x[], float y[], int n){ //two arrays, and the buffersize
+
+  //this is all just copied over from other cross correlation
+  float mx = 0, my = 0, sx = 0, sy = 0, sxy, r;
+    
+    // Calculate mean
+    for (int i = 0; i < n; i++) {
+        mx += x[i];
+        my += y[i];
+    }
+    mx /= n;
+    my /= n;
+
+    // calculate denominator
+    for (int i = 0; i < n; i++) {
+        sx += (x[i] - mx) * (x[i] - mx);
+        sy += (y[i] - my) * (y[i] - my);
+    }
+    double denom = sqrt(sx * sy);
+
+    int maxShift2 = 100; //this makes it match the matlab
+    const float Ts = 0.01; //sample rate
+    Serial.println("delay,correlation");  //Serial Plotter
+
+
+    float highr = -1.0;
+    float highshift = 0;
+    // Calculate and print cross-correlation
+    for (int shift = -maxShift2; shift <= maxShift2; shift++) { //og maxShift
+        sxy = 0;
+
+        for (int i = 0; i < n; i++) {
+            int j = i + shift;
+            if (j < 0 || j >= n) continue;
+            //if (j < 0 || j >= n){
+              //sxy += (x[i] - mx) * (-my);
+            //}
+            else{
+              sxy += (x[i] - mx) * (y[j] - my); 
+            }
+            
+        }
+
+        r = sxy / denom;
+
+        if (r > highr){
+          highr = r;
+          highshift = shift *Ts; //og just shift
+        }
+        float delaySec = shift * Ts; //og not here
+
+        Serial.print(delaySec, 5);
+        Serial.print(',');
+        Serial.println(r, 5);
+        delay(5);
+        
+      //need to return highr, highshift
+
+        delay(100); 
+
+        
 }
 
 float average_calculate(float arr[], int n){ //array and its size
